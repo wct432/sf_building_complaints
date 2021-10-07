be59e6de76e1b6a0061906d0f86839f57b3937a1
Willem Traupel <wctpro@gmail.com>
2021-10-07 13:52:23 -0700

    Updated LSTM Model and added notebook styling.

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,16 +1,12 @@
-import config #config file contains API credentials from data.sfgov
 import os
 import pandas as pd 
 from sodapy import Socrata
 
-os.chdir('..')
-parent_dir = os.getcwd()
-data_path = parent_dir + '/data/'
+working_dir = os.getcwd()
+data_path = os.path.dirname(working_dir) + '/data/'
 
-client = Socrata("data.sfgov.org", config.app_key)
-
-results = client.get("gm2e-bten", limit = 300000)
-
-results_df = pd.DataFrame.from_records(results, index ='complaint_number')
-
-results_df.to_csv(data_path + 'raw_data.csv')
+def fetch_data(app_key):
+    client = Socrata("data.sfgov.org", app_key)
+    results = client.get("gm2e-bten", limit = 300000)
+    results_df = pd.DataFrame.from_records(results, index ='complaint_number')
+    results_df.to_csv(data_path + 'raw_data.csv')
